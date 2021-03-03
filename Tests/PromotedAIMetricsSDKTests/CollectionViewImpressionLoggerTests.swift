4083a7ad227061dbb95bf5cccaf0a0722e6b969d
Yu-Hong Wang <limepi@gmail.com>
2021-03-02 20:21:36 -0800

    Create TestHelpers target. (#25)

--- Tests/PromotedAIMetricsSDKTests/CollectionViewImpressionLoggerTests.swift
+++ Tests/PromotedAIMetricsSDKTests/CollectionViewImpressionLoggerTests.swift
@@ -2,6 +2,7 @@ import Foundation
 import XCTest
 
 @testable import PromotedAIMetricsSDK
+@testable import TestHelpers
 
 final class CollectionViewImpressionLoggerTests: XCTestCase {
   class FakeDataSource: CollectionViewImpressionLoggerDataSource {
