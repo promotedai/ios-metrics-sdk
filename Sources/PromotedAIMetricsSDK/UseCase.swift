9f67d9c97cf9d58cd9511f0f23697d296506348d
Yu-Hong Wang <limepi@gmail.com>
2021-03-05 16:04:47 -0800

    Add UseCase enum.

---
+++ Sources/PromotedAIMetricsSDK/UseCase.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/** Use case for views. */
+@objc(PROUseCase)
+public enum UseCase: Int {
+  case search = 2
+  case searchSuggestions = 3
+  case feed = 4
+  case relatedContent = 5
+  case closeUp = 6
+  case categoryContent = 7
+  case myContent = 8
+  case savedContent = 9
+  case sellerContent = 10
+}
