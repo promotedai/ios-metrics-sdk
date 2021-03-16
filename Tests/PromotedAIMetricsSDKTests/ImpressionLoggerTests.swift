4b8dfe8c7649127603a23d3f6ccd8fda942c360c
Yu-Hong Wang <limepi@gmail.com>
2021-03-15 18:17:24 -0700

    ImpressionLogger changes to support Snackpass. (#52)

--- Tests/PromotedAIMetricsSDKTests/ImpressionLoggerTests.swift
+++ Tests/PromotedAIMetricsSDKTests/ImpressionLoggerTests.swift
@@ -29,6 +29,12 @@ final class ImpressionLoggerTests: XCTestCase {
     }
   }
   
+  class DataSource: ImpressionLoggerDataSource {
+    func impressionLoggerItem(at indexPath: IndexPath) -> Item? {
+      return nil
+    }
+  }
+  
   private func impression(_ item: IndexPath.Element,
                           _ startTime: TimeInterval,
                           _ endTime: TimeInterval = -1.0) -> ImpressionLogger.Impression {
@@ -41,83 +47,117 @@ final class ImpressionLoggerTests: XCTestCase {
                            _ b: [ImpressionLogger.Impression]) {
     XCTAssertEqual(Set(a), Set(b))
   }
+  
+  private var clock: FakeClock?
+  private var dataSource: DataSource?
+  private var delegate: Delegate?
+  private var idMap: IDMap?
+  private var metricsLogger: MetricsLogger?
+  private var impressionLogger: ImpressionLogger?
+  
+  public override func setUp() {
+    super.setUp()
+    clock = FakeClock()
+    dataSource = DataSource()
+    delegate = Delegate()
+    idMap = SHA1IDMap.instance
+    metricsLogger = MetricsLogger(messageProvider: FakeMessageProvider(),
+                                  clientConfig: ClientConfig(),
+                                  clock: clock!,
+                                  connection: FakeNetworkConnection(),
+                                  idMap: idMap!,
+                                  store: FakePersistentStore())
+    impressionLogger = ImpressionLogger(dataSource: dataSource!,
+                                        metricsLogger: metricsLogger!,
+                                        clock: clock!)
+    impressionLogger!.delegate = delegate
+  }
 
   func testStartImpressions() {
-    let delegate = Delegate()
-    let clock = FakeClock(now: 123)
-    let impressionLogger = ImpressionLogger(delegate: delegate, clock: clock)
+    clock!.advance(to: 123)
     
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 0))
-    assertContentsEqual(delegate.startImpressions, [impression(0, 123)])
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    assertContentsEqual(delegate!.startImpressions, [impression(0, 123)])
     
-    delegate.clear()
-    clock.now = 500
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 0))
-    clock.now = 501
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 1))
-    clock.now = 502
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 2))
-    assertContentsEqual(delegate.startImpressions,
+    delegate!.clear()
+    clock!.now = 500
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    clock!.now = 501
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 1))
+    clock!.now = 502
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 2))
+    assertContentsEqual(delegate!.startImpressions,
                         [impression(0, 500), impression(1, 501), impression(2, 502)])
   }
   
   func testEndImpressions() {
-    let delegate = Delegate()
-    let clock = FakeClock(now: 123)
-    let impressionLogger = ImpressionLogger(delegate: delegate, clock: clock)
+    clock!.advance(to: 123)
     
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 0))
-    clock.now = 200
-    impressionLogger.collectionViewDidHide(item: IndexPath(index: 0))
-    assertContentsEqual(delegate.endImpressions, [impression(0, 123, 200)])
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    clock!.now = 200
+    impressionLogger!.collectionViewDidHide(item: IndexPath(index: 0))
+    assertContentsEqual(delegate!.endImpressions, [impression(0, 123, 200)])
   }
   
   func testDidChangeImpressions() {
-    let delegate = Delegate()
-    let clock = FakeClock(now: 123)
-    let impressionLogger = ImpressionLogger(delegate: delegate, clock: clock)
+    clock!.advance(to: 123)
     
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 0))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 1))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 2))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 3))
-    assertContentsEqual(delegate.startImpressions,
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 1))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 2))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 3))
+    assertContentsEqual(delegate!.startImpressions,
                         [impression(0, 123), impression(1, 123),
                          impression(2, 123), impression(3, 123)])
 
-    delegate.clear()
+    delegate!.clear()
     let visibleItems = [IndexPath(index: 2), IndexPath(index: 3),
                         IndexPath(index: 4), IndexPath(index: 5)]
-    clock.now = 200
+    clock!.now = 200
     
-    impressionLogger.collectionViewDidChange(visibleItems: visibleItems)
-    assertContentsEqual(delegate.startImpressions,
+    impressionLogger!.collectionViewDidChange(visibleItems: visibleItems)
+    assertContentsEqual(delegate!.startImpressions,
                         [impression(4, 200), impression(5, 200)])
-    assertContentsEqual(delegate.endImpressions,
+    assertContentsEqual(delegate!.endImpressions,
                         [impression(0, 123, 200), impression(1, 123, 200)])
   }
   
   func testDidHideAllImpressions() {
-    let delegate = Delegate()
-    let clock = FakeClock(now: 123)
-    let impressionLogger = ImpressionLogger(delegate: delegate, clock: clock)
+    clock!.advance(to: 123)
     
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 0))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 1))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 2))
-    impressionLogger.collectionViewWillDisplay(item: IndexPath(index: 3))
-    assertContentsEqual(delegate.startImpressions,
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 1))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 2))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 3))
+    assertContentsEqual(delegate!.startImpressions,
                         [impression(0, 123), impression(1, 123),
                          impression(2, 123), impression(3, 123)])
 
-    delegate.clear()
-    clock.now = 200
+    delegate!.clear()
+    clock!.now = 200
     
-    impressionLogger.collectionViewDidHideAllItems()
-    assertContentsEqual(delegate.startImpressions, [])
-    assertContentsEqual(delegate.endImpressions,
+    impressionLogger!.collectionViewDidHideAllItems()
+    assertContentsEqual(delegate!.startImpressions, [])
+    assertContentsEqual(delegate!.endImpressions,
                         [impression(0, 123, 200), impression(1, 123, 200),
                          impression(2, 123, 200), impression(3, 123, 200)])
+  }
+  
+  func testArrayDataSource() {
+    let array = [Item(itemID: "id0"), Item(itemID: "id1"), Item(itemID: "id2")]
+    impressionLogger = ImpressionLogger(array: array,
+                                        metricsLogger: metricsLogger!,
+                                        clock: clock!)
+    
+    clock!.advance(to: 123)
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 0))
+    impressionLogger!.collectionViewWillDisplay(item: IndexPath(index: 1))
+
+    XCTAssertEqual(2, metricsLogger!.logMessages.count)
+    let impression0 = metricsLogger!.logMessages[0] as! Event_Impression
+    XCTAssertEqual(idMap!.impressionID(clientID: "id0"), impression0.impressionID)
+    let impression1 = metricsLogger!.logMessages[1] as! Event_Impression
+    XCTAssertEqual(idMap!.impressionID(clientID: "id1"), impression1.impressionID)
   }
 
   static var allTests = [
