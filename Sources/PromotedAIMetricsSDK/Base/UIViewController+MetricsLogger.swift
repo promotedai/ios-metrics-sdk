2dfd0f3903e7a8fd2774ce4626354200dcb53681
Yu-Hong Wang <limepi@gmail.com>
2021-04-13 22:08:42 -0700

    wip

--- Sources/PromotedAIMetricsSDK/Base/UIViewController+MetricsLogger.swift
+++ Sources/PromotedAIMetricsSDK/Base/UIViewController+MetricsLogger.swift
@@ -13,14 +13,6 @@ public extension UIViewController {
   @objc var promotedLoggerService: MetricsLoggerService {
     return MetricsLoggerService.shared
   }
-
-  /// Returns name used for logging views.
-  internal var promotedViewLoggingName: String {
-    let className = String(describing: type(of: self))
-    let loggingName = className.replacingOccurrences(of:"ViewController", with: "")
-    if loggingName.isEmpty { return "Unnamed" }
-    return loggingName
-  }
   
   /// Logs a view for self as a view controller.
   @objc func logPromotedViewForSelf() {
@@ -30,5 +22,15 @@ public extension UIViewController {
   /// Logs a view for self as a view controller and given use case.
   @objc func logPromotedViewForSelf(useCase: UseCase) {
     self.promotedLogger.logView(viewController: self, useCase: useCase)
+  }
+}
+
+extension UIViewController {
+  /// Returns name used for logging views.
+  var promotedViewLoggingName: String {
+    let className = String(describing: type(of: self))
+    let loggingName = className.replacingOccurrences(of: "ViewController", with: "")
+    if loggingName.isEmpty { return "Unnamed" }
+    return loggingName
   }
 }
