feabc862b20637f8f48de39ea032678f0b0bd164
Y Wang <limepi@gmail.com>
2021-07-07 14:10:01 -0700

    Add tests for String case conversions.

---
+++ Tests/PromotedFirebaseRemoteConfigTests/StringCaseConversionTests.swift
@@ -0,0 +1,25 @@
+import Foundation
+import XCTest
+
+@testable import PromotedFirebaseRemoteConfig
+
+final class StringCaseConversionTests: XCTestCase {
+
+  func testSnakeCase() {
+    XCTAssertEqual("json", "json".toSnakeCase())
+    XCTAssertEqual("batch_summaries", "batchSummaries".toSnakeCase())
+    XCTAssertEqual(
+      "call_details_and_stack_traces",
+      "callDetailsAndStackTraces".toSnakeCase()
+    )
+  }
+
+  func testCamelCase() {
+    XCTAssertEqual("json", "json".toCamelCase())
+        XCTAssertEqual("batchSummaries", "batch_summaries".toCamelCase())
+        XCTAssertEqual(
+          "callDetailsAndStackTraces",
+          "call_details_and_stack_traces".toCamelCase()
+        )
+  }
+}
