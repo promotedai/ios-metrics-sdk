71fd486b59907d3dc9bde100cd72580cfeafe5b2
Y Wang <yu-hong@promoted.ai>
2021-12-03 13:00:42 -0800

    Fix tests again

--- Tests/PromotedCoreTests/IOSBuildInfoTests.swift
+++ Tests/PromotedCoreTests/IOSBuildInfoTests.swift
@@ -24,10 +24,11 @@ final class IOSBuildInfoTests: XCTestCase {
     let version = build.platformAppVersion
     let parts = version.split(separator: " ").map { String($0) }
     XCTAssertEqual(3, parts.count)
-    XCTAssertNotEqual("Unknown", parts[0])
-    assert(regex: #"\d+\.\d+"#, matches: parts[0])
-    XCTAssertEqual("build", parts[1])
     // Github Actions sometimes return "Unknown" in tests.
+    if parts[0] != "Unknown" {
+      assert(regex: #"\d+\.\d+"#, matches: parts[0])
+    }
+    XCTAssertEqual("build", parts[1])
     if parts[2] != "Unknown" {
       assert(regex: #"\d+"#, matches: parts[2])
     }
