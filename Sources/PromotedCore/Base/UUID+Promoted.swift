6315212d3bd844419627a3d3bf1b040615fde07d
Y Wang <yu-hong@promoted.ai>
2021-12-02 17:08:58 -0800

    Fix distribution

--- Sources/PromotedCore/Base/UUID+Promoted.swift
+++ Sources/PromotedCore/Base/UUID+Promoted.swift
@@ -3,23 +3,39 @@ import Foundation
 extension UUID {
   /// Generates a hash value that is stable across invocations
   /// of the process. (In general, hash values in Swift aren't
-  /// stable, including UUID's hash value.)
-  var stableHashValue: UInt64 {
+  /// stable across different runs of the process.)
+  var stableHashValue: (UInt64, UInt64) {
     let a = self.uuid
-    return (
+    let hi = (
       UInt64(a.0 << 56) |
-      UInt64(a.2 << 48) |
-      UInt64(a.4 << 40) |
-      UInt64(a.6 << 32) |
-      UInt64(a.8 << 24) |
-      UInt64(a.10 << 16) |
-      UInt64(a.12 << 8) |
-      UInt64(a.14)
+      UInt64(a.1 << 48) |
+      UInt64(a.2 << 40) |
+      UInt64(a.3 << 32) |
+      UInt64(a.4 << 24) |
+      UInt64(a.5 << 16) |
+      UInt64(a.6 << 8) |
+      UInt64(a.7)
     )
+    let lo = (
+      UInt64(a.8 << 56) |
+      UInt64(a.9 << 48) |
+      UInt64(a.10 << 40) |
+      UInt64(a.11 << 32) |
+      UInt64(a.12 << 24) |
+      UInt64(a.13 << 16) |
+      UInt64(a.14 << 8) |
+      UInt64(a.15)
+    )
+    return (hi, lo)
   }
 
   /// Computes stable hash value over given modulus.
-  func stableHashValueMod(_ modulus: UInt) -> UInt {
-    return UInt(stableHashValue % UInt64(modulus))
+  func stableHashValueMod(_ modulus: UInt32) -> UInt32 {
+    let (hi, lo) = stableHashValue
+    let mod64 = UInt64(modulus)
+    let max64mod = UInt64.max % mod64
+    return UInt32((
+      ((hi % mod64) * (max64mod)) + (lo % mod64)
+    ) % mod64)
   }
 }
