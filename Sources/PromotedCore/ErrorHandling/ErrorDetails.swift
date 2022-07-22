bbe347b7ea99ca5cc2aec38369bcc00ff071118e
Yuna Wang <yuna@promoted.ai>
2022-07-22 15:36:57 -0700

    More boilerplate

--- Sources/PromotedCore/ErrorHandling/ErrorDetails.swift
+++ Sources/PromotedCore/ErrorHandling/ErrorDetails.swift
@@ -5,6 +5,30 @@ public protocol ErrorDetails {
   var details: String { get }
 }
 
+// MARK: - String Constants
+fileprivate extension ErrorDetails {
+
+  static let internalErrorDetails = """
+  An internal error occurred in the Promoted SDK. Please email bugs@promoted.ai and provide the error code.
+  """
+
+  static let deliveryMayBeImpaired = """
+  If this issue is released to production, it WILL impair Promoted Delivery and possibly affect revenue at $partner. Please verify any local changes carefully before merging.
+  """
+
+  static let deliveryWillBeDisabled = """
+  If this issue is released to production, it WILL DISABLE Promoted Delivery and possibly affect revenue at $partner. Please verify any local changes carefully before merging.
+  """
+
+  static let checkRemoteConfigSetup = """
+  Please check your Remote Config setup. In a production build, the Promoted SDK will start up and run normally, but Remote Config will not be available.
+  """
+
+  static let checkRemoteConfigEnumValues = """
+  If you are using Remote Config, check that you specified a valid enumeration value.
+  """
+}
+
 // MARK: - ClientConfigError
 extension ClientConfigError: ErrorDetails {
   public var details: String {
@@ -12,11 +36,16 @@ extension ClientConfigError: ErrorDetails {
     case .invalidURL(let url):
       return """
       An invalid URL was supplied for `ClientConfig.metricsLoggingURL`:
+
       \(url)
+
+      \(Self.deliveryWillBeDisabled)
       """
     case .missingAPIKey:
       return """
       No API key was provided via `ClientConfig.metricsLoggingAPIKey`.
+
+      \(Self.deliveryWillBeDisabled)
       """
     case .missingDevAPIKey:
       return """
@@ -24,25 +53,28 @@ extension ClientConfigError: ErrorDetails {
       """
     case .invalidMetricsLoggingWireFormat:
       return """
-      Invalid value for enum `ClientConfig.metricsLoggingWireFormat`.
+      Invalid value for enum `ClientConfig.metricsLoggingWireFormat`. Defaulting to `.binary`. \(checkRemoteConfigEnumValues)
       """
     case .invalidXrayLevel:
       return """
-      Invalid value for enum `ClientConfig.xrayLevel`.
+      Invalid value for enum `ClientConfig.xrayLevel`. Xray will be disabled. \(checkRemoteConfigEnumValues)
       """
     case .invalidOSLogLevel:
       return """
-      Invalid value for enum `ClientConfig.osLogLevel`.
+      Invalid value for enum `ClientConfig.osLogLevel`. Console logging will be disabled. \(checkRemoteConfigEnumValues)
       """
     case .invalidDiagnosticsSamplingEndDateString(let s):
       return """
       You indicated an end date for diagnostic sampling that does not appear to be in the format yyyy-MM-dd:
 
       \(s)
+
+      Diagnostics sampling will be disabled.
       """
     case .invalidMetricsLoggingErrorHandling:
+      // This will only appear in debug builds, where the default is `.modalDialog`.
       return """
-      Invalid value for enum `ClientConfig.metricsLoggingErrorHandling`.
+      Invalid value for enum `ClientConfig.metricsLoggingErrorHandling`. Error handling will be set to `.modalDialog`. \(checkRemoteConfigEnumValues)
       """
     }
   }
@@ -55,6 +87,8 @@ extension ModuleConfigError: ErrorDetails {
     case .missingNetworkConnection:
       return """
       Network connection not specified. You are using `ModuleConfig.coreConfig()`, which requires a custom network connection. Did you mean to use `ModuleConfig.defaultConfig()`?
+
+      \(Self.deliveryWillBeDisabled)
       """
     }
   }
@@ -62,34 +96,25 @@ extension ModuleConfigError: ErrorDetails {
 
 // MARK: - MetricsLoggerError
 extension MetricsLoggerError: ErrorDetails {
-
-  static let internalErrorDetails = """
-  An internal error occurred in the Promoted SDK. Please email bugs@promoted.ai and provide the error code.
-  """
-
-  static let revenueWarning = """
-  If this issue is released to production, it WILL impair Promoted Delivery and possibly affect revenue at $partner. Please verify any local changes carefully before merging.
-  """
-
   public var details: String {
     switch self {
     case .missingLogUserIDInUserMessage, .missingLogUserIDInLogRequest:
       return """
       An event was logged with no log user ID. This may be due to a recent change in Promoted initialization. Examples include changes to your AppDelegate class, or calls to the Promoted SDK method startSessionAndLogUser().
 
-      \(Self.revenueWarning)
+      \(Self.deliveryMayBeImpaired)
       """
     case .missingJoinableFieldsInImpression:
       return """
       An impression was logged with no content ID or insertion ID. This may be due to a recent change to a list/collection view, or the data model that powers this view.
 
-      \(Self.revenueWarning)
+      \(Self.deliveryMayBeImpaired)
       """
     case .missingJoinableFieldsInAction:
       return """
       An action was logged with no impression ID, content ID, or insertion ID. This may be due to a recent change to a list/collection view, or the data model that powers this view.
 
-      \(Self.revenueWarning)
+      \(Self.deliveryMayBeImpaired)
       """
     case .propertiesSerializationError(_),
          .calledFromWrongThread,
@@ -101,10 +126,6 @@ extension MetricsLoggerError: ErrorDetails {
 
 // MARK: - ClientConfigFetchError
 extension ClientConfigFetchError: ErrorDetails {
-
-  static let checkRemoteConfigSetup = """
-  Please check your Remote Config setup. In a production build, the Promoted SDK will start up and run normally, but Remote Config will not be available.
-  """
   public var details: String {
     switch self {
     case .networkError(let error):
@@ -133,9 +154,6 @@ extension ClientConfigFetchError: ErrorDetails {
 
 // MARK: - RemoteConfigConnectionError
 extension RemoteConfigConnectionError: ErrorDetails {
-
-  static let checkRemoteConfigSetup = ClientConfigFetchError.checkRemoteConfigSetup
-
   public var details: String {
     switch self {
     case .failed(let maybeError):
